2f17c4a3531acf40222d714621f24332d6c24307
zer0_1s <chenyu3050@163.com>
2020-12-16 21:25:14 +0800

    a small mistake

--- python_test/demo0/client_tcp.py
+++ python_test/demo0/client_tcp.py
@@ -5,7 +5,7 @@ target_port=80
 
 # set an socket object
 '''
-AF_INET represnet standard IPV4 address or hostname.
+AF_INET represent standard IPV4 address or hostname.
 SOCK_STREAM represent TCP client.
 TCP (SOCK_STREAM) is a connection-based protocol. The connection is established and the two parties have a conversation until the connection is terminated by one of the parties or by a network error.
 '''
