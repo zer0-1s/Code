7be31e6f9c80f4a5ab0c6b207d86ea005f0e4e54
zer0_1s <chenyu3050@163.com>
2020-12-16 20:36:33 +0800

    finish first set tcp client with python

---
+++ python_test/demo0/client_tcp.py
@@ -0,0 +1,20 @@
+import socket
+
+target_host="www.baidu.com"
+target_port=80
+
+# set an socket object
+'''
+AF_INET represnet standard IPV4 address or hostname.
+SOCK_STREAM represent TCP client.
+TCP (SOCK_STREAM) is a connection-based protocol. The connection is established and the two parties have a conversation until the connection is terminated by one of the parties or by a network error.
+'''
+client =socket .socket(socket.AF_INET,socket.SOCK_STREAM)
+# link to client
+client.connect((target_host,target_port))
+# sent some data to target ip
+client.send(b"GET / HTTP/1.1\r\nHost:baidu.com\r\n\r\n")
+# recvive some data
+response=client.recv(4096)
+
+print (response)
